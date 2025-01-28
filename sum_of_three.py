29305b5e9eafc0cf47536c3d5c845b64cdff7a46
andy heggs <andy@heggs.net>
2025-01-28 10:23:10 +0000

    Solve first Python exercise of the day

--- sum_of_three.py
+++ sum_of_three.py
@@ -4,4 +4,6 @@ def sum3(first_number, second_number, third_number):
     """
     Takes three numbers and returns the sum of those numbers
     """
-    pass  # YOUR CODE HERE
+
+    return (first_number + second_number + third_number)
+print(sum3(2, 2, 2))
